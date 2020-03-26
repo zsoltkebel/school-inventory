aee98b2d25adc1d722c02cf1511e33caaa965ca5
Zsolt Kébel <kebel.zsolt@gmail.com>
2020-03-26 15:17:42 +0100

    code cleanup

--- src/main/java/inventory/utils/ReflectionUtil.java
+++ src/main/java/inventory/utils/ReflectionUtil.java
@@ -22,15 +22,19 @@ public class ReflectionUtil {
      * @return Fields of the class
      */
     public static List<Field> getFields(Class<?> from, Class<?> until, String... except) {
+        // return an empty list if the until class is not a superclass of from
         if (!until.isAssignableFrom(from)) return new ArrayList<>();
 
         List<Field> fields = new ArrayList<>();
 
         if (!from.equals(until)) {
+            // recursively call this function on the superclass
             fields.addAll(getFields(from.getSuperclass(), until));
         }
+        // retrieve fields of the class
         fields.addAll(Arrays.asList(from.getDeclaredFields()));
 
+        // remove those that are excluded
         fields.removeIf(field -> Arrays.asList(except).contains(field.getName()));
         fields.forEach(field -> field.setAccessible(true));
         return fields;
